dcd7302e7970f3e1174306e4e0ec105f7cef8d70
Carlos Alberto <carloscastelo@ifce.edu.br>
2025-08-05 16:55:35 +0000

    feat: add instructions panel for PDF processing workflow in the main view

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="container mx-auto px-2 sm:px-4 py-6 flex flex-col gap-4 w-full items-center">
+        <Panel toggleable collapsed header="Instruções">
+            <ol class="list-decimal list-inside flex flex-col gap-2">
+                <li>Carregue o PDF com a ficha de fotos.</li>
+                <li>Use os sliders da configuração de corte e mantenha o quadro laranja sobre todas as fotos.</li>
+                <li>Use os últimos 2 sliders para ajustar a área de legenda para que o quadro amarelo esteja sobre todas as legendas.</li>
+                <li>Use o painel de preview para ver como vai ficar a foto e a leitura da legenda abaixo.</li>
+                <li>Se estiver satisfeito, clique em "Processar" e depois baixe o arquivo zip contendo todas as fotos nomeadas.</li>
+            </ol>
+        </Panel>
         <div>
             <LoadPDF @loaded="onPdfLoad"/>
         </div>
